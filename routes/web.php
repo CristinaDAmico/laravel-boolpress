d48a493d4f6d6828c63e786574e089171d9c1435
CristinaDAmico <crydamico94@gmail.com>
2021-06-17 17:26:19 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
-
 // ROTTE PER AUTENTICAZIONE
 Auth::routes();
 
@@ -34,3 +30,8 @@ Route::prefix('admin')
         // Rotte resource posts
         Route::resource('/posts', 'PostController');
     });
+
+// Front office
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".*");
