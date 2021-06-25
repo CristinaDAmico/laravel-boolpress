3e770a5ab300a988328f0d2a99059fb3ed85f322
CristinaDAmico <crydamico94@gmail.com>
2021-06-25 13:03:39 +0200

    commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PostsTableSeeder::class,
             CategoriesTableseeder::class,
+            TagsTableSeeder::class,
         ]);
     }
 }
