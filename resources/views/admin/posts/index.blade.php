124173389cb17ef72977cb0dcc0d03b5045ddc75
CristinaDAmico <crydamico94@gmail.com>
2021-06-28 19:06:30 +0200

    commit

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -18,6 +18,7 @@
                     <th>ID</th>
                     <th>Title</th>
                     <th>Category</th>
+                    <th>Create</th>
                     <th colspan="3">Actions</th>
                 </tr>
             </thead>
@@ -31,6 +32,12 @@
                                 {{ $post->category->name }}
                             @endif
                         </td>
+
+                        <td>
+                            <div>{{ $post->created_at->format('l d/m/y') }}</div>
+                            <div>{{ $post->created_at->diffForHumans() }}</div>  
+                        </td>
+
                         <td>
                             <a class="btn btn-success" href="{{ route('admin.posts.show', $post->id )}}">SHOW</a>
                         </td>
